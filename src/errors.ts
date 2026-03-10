ff1e0949ac38d3f452c14419107a04eb675499db
Rinat <you@example.com>
2026-03-10 14:50:32 +0500

    removed shit

--- src/errors.ts
+++
@@ -1,9 +0,0 @@
-export const PROVIDER_ERROR_CODES: Set<number> = new Set([
-  4001,
-  4100,
-  4200,
-  4900,
-  4901,
-  -32603,
-  -32000,
-]);
--- src/favicon.ts
+++
@@ -1,4 +0,0 @@
-export function getFavicon(): string {
-  const ref = globalThis.document?.querySelector<HTMLLinkElement>("link[rel*='icon']");
-  return ref?.href ?? "";
-}
